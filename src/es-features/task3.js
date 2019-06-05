0d85b28a315de8eb7bb58c571b9a1c354efdc009
mche90 <marichirikovava@gmail.com>
2019-06-05 12:24:56 +0300

    Result for the task 4

--- src/es-features/task3.js
+++ src/es-features/task3.js
@@ -9,14 +9,14 @@
 
 export function task3Old(params) {
   return [1, 2].concat(params);
-} 
+}
 
 // Напишите реализацию функции task3Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task3New () {
-const first_value = [1, 2];
-const second_value = ['go', 5, 11, 'back']; 
-const end = Array.from(new Set(first_value.concat(second_value)));
-return end;
+export function task3New() {
+  const first_value = [1, 2];
+  const second_value = ['go', 5, 11, 'back'];
+  const end = Array.from(new Set(first_value.concat(second_value)));
+  return end;
 }
